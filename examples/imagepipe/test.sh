2421e33d2776e8879dcf2f2d53add3bb07643512
Erik Schomburg <eschomburg@flatironinstitute.org>
2024-12-09 15:43:22 -0500

    add commented test code variations

--- examples/imagepipe/test.sh
+++ examples/imagepipe/test.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-imagepipe --pdb-step \
+
+#imagepipe \
+#    open -i ./example01.jpg -i ./example02.jpg \
+#    display
+
+
+imagepipe \
     open -i ./example01.jpg -i ./example02.jpg \
     transpose -r 180 \
     emboss \
     display
+
+
+#imagepipe --pdb-step \
+#    open -i ./example01.jpg -i ./example02.jpg \
+#    transpose -r 180 \
+#    emboss \
+#    display
